cf209c385319bfdf1f43be819fb37f46a79cb542
FrancisMosoti <francismosoti20@gmail.com>
2025-08-08 12:04:04 +0300

    Footer section

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
-import { HomePage } from "../App";
+
 import Navbar from "../Sections/Navbar";
+import Footer from "../Sections/Footer";
 
 const Home = () => {
   return (
     <div className="bg-[#F5F5F5] min-h-screen">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
