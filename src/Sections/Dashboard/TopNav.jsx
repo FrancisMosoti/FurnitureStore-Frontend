c38985c207b441deebcee634346c7460be3eca72
FrancisMosoti <francismosoti20@gmail.com>
2025-08-25 00:44:16 +0300

    TopNav

--- src/Sections/Dashboard/TopNav.jsx
+++ src/Sections/Dashboard/TopNav.jsx
@@ -2,7 +2,7 @@ import { CgProfile } from "react-icons/cg";
 
 const TopNav = () => {
   return (
-    <div className="w-full fixed bg-white/20 backdrop-blur-sm border border-white/30 shadow-lg flex flex-col sm:flex-row justify-between items-start sm:items-center p-3  gap-2">
+    <div className="w-full fixed bg-white/20 backdrop-blur-sm border border-white/30 shadow-lg flex flex-col sm:flex-row justify-between items-start sm:items-center p-3  gap-2 pl-16 md:pl-3">
       {/* Greeting */}
       <h1 className="text-xl sm:text-2xl font-bold text-gray-900 drop-shadow-sm">
         Hello, Welcome back!
