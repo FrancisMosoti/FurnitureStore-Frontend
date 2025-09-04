a2aae3e56650b6d00084c7848742d64fc74ee83d
FrancisMosoti <francismosoti20@gmail.com>
2025-09-04 12:14:54 +0300

    Fixed pagination component on table

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -14,37 +14,39 @@ const Table = ({ columns, data, rowsPerPage = 5 }) => {
     }
   };
   return (
-    <div className="overflow-x-auto p-3 mt-6 rounded-lg border border-gray-200 ">
-      <table className="w-full text-left border-collapse">
-        <thead className="bg-gray-300">
-          <tr>
-            {columns.map((col) => (
-              <th
-                key={col.accessor}
-                className="px-6 py-3 text-sm font-medium text-gray-600"
-              >
-                {col.header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="">
-          {currentData.map((row, idx) => (
-            <tr key={idx} className={`hover:bg-[#365A4C]/10 `}>
+    <div className="p-3 mt-6 rounded-lg border border-gray-200 ">
+      <div className="overflow-x-auto ">
+        <table className="w-full text-left border-collapse">
+          <thead className="bg-gray-300">
+            <tr>
               {columns.map((col) => (
-                <td
+                <th
                   key={col.accessor}
-                  className="px-6 py-4 text-medium font-medium text-gray-700"
+                  className="px-6 py-3 text-sm font-medium text-gray-600"
                 >
-                  {col.render
-                    ? col.render(row[col.accessor], row)
-                    : row[col.accessor]}
-                </td>
+                  {col.header}
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="">
+            {currentData.map((row, idx) => (
+              <tr key={idx} className={`hover:bg-[#365A4C]/10 `}>
+                {columns.map((col) => (
+                  <td
+                    key={col.accessor}
+                    className="px-6 py-4 text-medium font-medium text-gray-700"
+                  >
+                    {col.render
+                      ? col.render(row[col.accessor], row)
+                      : row[col.accessor]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       {/* Pagination controls */}
       <div className="flex items-center justify-between mt-4 font-medium">
         <p className="text-sm text-gray-600">
